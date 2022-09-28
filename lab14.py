483f5da236ddad1f993280fef5a88ba7794edba4
miggiegarciadev <miggiegarciadev@gmail.com>
2022-09-28 19:54:54 +0000

    lab 14 started

---
+++ lab14.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env python3
+
+def main ():
+
+user = input("User's Name: ")
+day = input("Day of Week: ")
+
+print ("Hello, " <user> "!" "Happy " <day of the week> "!")
+               
