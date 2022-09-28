e9f135a50623c6f8bd5dd11e3c472437f45a2aa9
miggiegarciadev <miggiegarciadev@gmail.com>
2022-09-28 20:51:50 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,24 @@
+# tells computer torun code?? 
+
+#!/usr/bin/env python3
+
+# making list with three things on it
+# my_list = [ "192.168.0.5", 5060, "UP" ]
+
+# shhows the first thing on list 
+# print("The first item in the list (IP): " + my_list[0] )
+
+# shows the second thing on list
+# print("The second item in the list (port): " + str(my_list[1]) )
+
+# shows the third thing  on the 
+# print("The second item in the list (port): " + str(my_list[2]) )
+
+# making a list and assign it to iplist variable
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+#display only ip addresses
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+
+
+
