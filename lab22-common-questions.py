a43e2f3f116886a859931a83cb6ce9b3ac810fd2
miggiegarciadev <miggiegarciadev@gmail.com>
2022-09-29 16:53:43 +0000

    q and a from bottom of lab 22

---
+++ lab22-common-questions.py
@@ -0,0 +1,9 @@
+Q: What are functions?
+A: References to defined code. Functions allow designers to write less, and increase the manageability of code as your project grows in complexity.
+Q: What is a class?
+A: A class creates objects. They're a bit like object factories.
+Q: What is a method?
+A: Classes build objects. A method is just a function defined within a class. Therefore, when you create an object (like a string or list), they inherit the functions defined within their class.
+Q: Do I have to understand classes, objects, and functions to be successful with Python?
+A: Not at all. Understanding how objects are initialized, and then interact with functions and methods can help you make better sense of pip, import statements, dot notation, and writing more efficient code. However, you'll learn that stuff as you work with Python. Don't let it be a barrier to picking up Python to solve a problem.
+
