497b143b5dea141a302e7538b837f8db25778738
miggiegarciadev <miggiegarciadev@gmail.com>
2022-10-03 20:33:47 +0000

    perfect pizza graph

---
+++ perfectPizza/perfectPizza.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# Compute areas and colors
+N = 150
+r = 2 * np.random.rand(N)
+theta = 2 * np.pi * np.random.rand(N)
+area = 200 * r**2
+colors = theta
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='polar')
+c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
+
+  plt.savefig("/home/student/mycode/perfectPizza/perfectPizza.png")
+
+   plt.savefig("/home/student/mycode/perfectPizza/static/perfectPizza.png")
+    print("Graph created.")
+
+    if __name__ == "__main__":
+        main()
