9aea4ccc71bde36e0c1b2899763285502cc53494
miggiegarciadev <miggiegarciadev@gmail.com>
2022-10-03 20:49:41 +0000

    perfect pizza with graph actually printing

--- perfectPizza/perfectPizza.py
+++ perfectPizza/perfectPizza.py
@@ -1,25 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Fixing random state for reproducibility
 np.random.seed(19680801)
 
-# Compute areas and colors
-N = 150
-r = 2 * np.random.rand(N)
-theta = 2 * np.pi * np.random.rand(N)
-area = 200 * r**2
-colors = theta
+def main():
+    # Compute areas and colors
+    N = 150
+    r = 2 * np.random.rand(N)
+    theta = 2 * np.pi * np.random.rand(N)
+    area = 200 * r**2
+    colors = theta
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='polar')
-c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='polar')
+    c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
 
-  plt.savefig("/home/student/mycode/perfectPizza/perfectPizza.png")
 
-   plt.savefig("/home/student/mycode/perfectPizza/static/perfectPizza.png")
+    #saving the graphhy locally
+    plt.savefig("/home/student/mycode/perfectPizza/perfectPizza.png")
+
+    #saving graph to static
+    plt.savefig("/home/student/static/perfectPizza.png")
     print("Graph created.")
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
